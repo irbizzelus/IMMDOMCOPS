05533bc89b140d5f3ae2a61c939522a17730d7d0
irbizzelus <117110119+irbizzelus@users.noreply.github.com>
2022-11-02 21:07:24 +0300

    adding Brazilian Portuguese by gabsF

---
+++ Immortal dominated cops/lua/menu.lua
@@ -0,0 +1,30 @@
+if not immdomcops then
+    _G.immdomcops = {}
+	immdomcops.modpath = ModPath
+	immdomcops.hostages = {}
+	immdomcops.settings = {
+		highlights = true,
+		meleekill = true
+    }
+
+    function immdomcops:Save()
+        local file = io.open(SavePath .. 'IMMDOMCOP_save.txt', 'w+')
+        if file then
+            file:write(json.encode(immdomcops.settings))
+            file:close()
+        end
+    end
+    
+    function immdomcops:Load()
+        local file = io.open(SavePath .. 'IMMDOMCOP_save.txt', 'r')
+        if file then
+            for k, v in pairs(json.decode(file:read('*all')) or {}) do
+                immdomcops.settings[k] = v
+            end
+            file:close()
+        end
+    end
+	
+	immdomcops:Load()
+    immdomcops:Save()	
+end
---
+++ Immortal dominated cops/menus/immdomcopsmenu.lua
@@ -0,0 +1,54 @@
+dofile(ModPath .. "lua/menu.lua")
+
+Hooks:Add('LocalizationManagerPostInit', 'immdomcops_loc', function(loc)
+	immdomcops:Load()
+	
+	local lang = "en"
+	local file = io.open(SavePath .. 'blt_data.txt', 'r')
+    if file then
+        for k, v in pairs(json.decode(file:read('*all')) or {}) do
+			if k == "language" then
+				lang = v
+			end
+        end
+        file:close()
+    end
+
+	if lang == "ru" then
+		loc:load_localization_file(immdomcops.modpath .. 'menus/lang/immdomcopsmenu_ru.txt', false)
+	elseif lang == "fr" then -- thanks Tardoss
+		loc:load_localization_file(immdomcops.modpath .. 'menus/lang/immdomcopsmenu_fr.txt', false)
+	elseif lang == "chs" then -- thanks Arknights
+		loc:load_localization_file(immdomcops.modpath .. 'menus/lang/immdomcopsmenu_chs.txt', false)
+	elseif lang == "es" then -- thanks MaxiAccess
+		loc:load_localization_file(immdomcops.modpath .. 'menus/lang/immdomcopsmenu_es.txt', false)
+	elseif lang == "pt-br" then -- thanks gabsF
+		loc:load_localization_file(immdomcops.modpath .. 'menus/lang/immdomcopsmenu_pt-br.txt', false)
+	else
+		loc:load_localization_file(immdomcops.modpath .. 'menus/lang/immdomcopsmenu_en.txt', false)
+	end
+end)
+
+Hooks:Add('MenuManagerInitialize', 'immdomcops_init', function(menu_manager)
+	MenuCallbackHandler.idc_immdomcopssave = function(this, item)
+		immdomcops:Save()
+	end
+	
+	MenuCallbackHandler.idc_donothing = function(this, item)
+		-- do nothing
+	end
+
+	MenuCallbackHandler.idc_cb_highlights = function(this, item)
+		immdomcops.settings[item:name()] = item:value() == 'on'
+		immdomcops:Save()
+	end
+	
+	MenuCallbackHandler.idc_cb_meleekill = function(this, item)
+		immdomcops.settings[item:name()] = item:value() == 'on'
+		immdomcops:Save()
+	end
+
+	immdomcops:Load()
+
+	MenuHelper:LoadFromJsonFile(immdomcops.modpath .. 'menus/immdomcopsmenu.txt', immdomcops, immdomcops.settings)
+end)
